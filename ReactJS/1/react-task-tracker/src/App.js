5cd4a754d9efb7034a0177b044325d6a540ab79a
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-08-29 11:20:15 +0530

    react component

--- ReactJS/1/react-task-tracker/src/App.js
+++ ReactJS/1/react-task-tracker/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+const name ="Devill Tyagi"
+const x= true;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello dosto</h1>
+<h1>{name}</h1>      
+      <h2>{55+6}</h2>
+      <h2> { x ? "Yes" : "No"}</h2>
     </div>
   );
 }
