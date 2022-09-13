eef4f2aceac716d13628a969b769c097580df2fa
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-09-13 14:42:51 +0530

    Project react

---
+++ ReactJS/Project_One/Project_One/src/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form=()=> {
+  return (
+    <div>
+        <h1>This is App component,Heading One</h1>
+        <h2>This is App component,Heading two</h2>
+        <p>This is App component</p>
+        <Form/>
+        </div>
+  );
+}
+
+export default Form;
---
+++ ReactJS/Project_One/Project_One/src/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Test.css';
+ export default function Test() {
+    return(
+  <div>
+    <h1 className="one">This is the test component</h1>
+    <h1 className="two">This is the test component</h1>
+    <h1 className="three">This is the test component</h1>
+  </div>
+    )
+}
+
