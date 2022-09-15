b34d7a819b80fefa35889f52029fa595b3ddc3c2
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-09-15 11:22:43 +0530

    react css

---
+++ ReactJS/Project_two/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import "./App.css";
+//import Home  from './Home/Home';
+import Index from './Component/index';
+import "bootstrap/dist/css/bootstrap.min.css";
+const App=()=>{
+  return (
+    <div className='App'>
+      <Index/>      
+      <h1>Hello world </h1>
+      <h2>This is heading 2</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/Project_two/src/Component/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css";
+
+const index=()=> {
+  return (
+    <div>
+        <h1 style={{color:"blue"}}>Hiii , How are u bro ?</h1>
+        <p style={{color:"voilet"}}>I m fine u ,tell me how do u do </p>
+    </div>
+  )
+}
+
+export default index; 
