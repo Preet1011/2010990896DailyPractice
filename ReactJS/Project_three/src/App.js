f177e7a6dcb843bd9d292b2b38b08e3f836ba4d0
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-09-19 11:21:04 +0530

    form in react

---
+++ ReactJS/Project_three/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Form from './Component/Form';
+
+//import "./App.css";
+//import Home  from './Home/Home';
+import Index from './Component/index';
+import "bootstrap/dist/css/bootstrap.min.css";
+const App=()=>{
+  return (
+    <div className='App'>
+      <Form/>      
+      <h1>Hello world </h1>
+      <h2>This is heading 2</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/Project_three/src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.css";
+
+ const Home=()=> {
+  return (
+    <div>
+        <h1>Devill Tyagi</h1>
+        <h2>Tyagi Boy</h2>
+        
+    </div>
+  )
+}
+
+export default Home;
