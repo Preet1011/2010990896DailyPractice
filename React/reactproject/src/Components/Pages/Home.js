fdd4c79127c51521b71b777c1f5b95bdf92408dc
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-10-09 23:52:33 +0530

    update

--- React/reactproject/src/Components/Pages/Home.js
+++ React/reactproject/src/Components/Pages/Home.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React , {useState ,useEffect}from "react";
+import axios from "axios";
 const Home =() => {
+    const [users, setUsers] = useState([]);
+    useEffect = ()=>{
+        console.log("How r u? ")
+
+    }
     return (
         <div className="container">
             <div className="py-4">
