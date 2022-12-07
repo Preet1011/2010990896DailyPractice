9e90bd8370b5c42adc4e018d5cbecf75f1bb3145
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2022-12-07 23:08:54 +0530

    conversion

---
+++ Basic to class type conversion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+ class Integer
+ {
+ 	int i ;
+ 	public:
+ 	void display()
+ 	{
+ 		cout<<"The valude of integer is : "<<i<<endl;
+	 }
+	 Integer (int i){
+	 	this -> i=i;
+	 }
+	 operator int(){
+	 	return i;
+	 }
+ };
+ 
+ int main(){
+ 	Integer J=100;
+ 	J.display();
+ 	int y =J;
+ 	cout<<"The value of y is :"<<y<<endl;
+ 	return 0;
+ }
